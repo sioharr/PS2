546ff10caf6b53fe5b3fc3a2cc037aad0f5e652c
sioharr <31242932+sioharr@users.noreply.github.com>
2021-01-22 05:15:31 -0700

    Update entrypoint.sh
    Actual entrypoint code.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-# Container image that runs your code
-FROM alpine:3.10
+#!bin/sh -1
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
-
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
+echo "Hi $1"
+time=$(date)
+echo "::set-output name=time::$time"
